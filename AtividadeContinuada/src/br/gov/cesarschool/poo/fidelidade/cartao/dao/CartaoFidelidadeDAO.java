fbc59ade7cddfc726cf4d23100b02fb3c1008af0
NicoleVictory <99563517+NicoleVictory@users.noreply.github.com>
2023-04-06 21:19:18 -0300

    Delete everything for a new Dir

--- AtividadeContinuada/src/br/gov/cesarschool/poo/fidelidade/cartao/dao/CartaoFidelidadeDAO.java
+++
@@ -1,5 +0,0 @@
-package br.gov.cesarschool.poo.fidelidade.cartao.dao;
-
-public class CartaoFidelidadeDAO {
-
-}
--- AtividadeContinuada/src/br/gov/cesarschool/poo/fidelidade/cartao/negocio/LancamentoExtratoPontuacao.java
+++
@@ -1,8 +0,0 @@
-package br.gov.cesarschool.poo.fidelidade.cartao.negocio;
-import  java.util.Date;
-
-public class LancamentoExtratoPontuacao extends LancamentoExtrato{
-	public LancamentoExtratoPontuacao(long numeroCartao, int quantidadePontos, Date dataHoraAtualizacao){
-		super(numeroCartao, quantidadePontos, dataHoraAtualizacao);
-	}
-}
