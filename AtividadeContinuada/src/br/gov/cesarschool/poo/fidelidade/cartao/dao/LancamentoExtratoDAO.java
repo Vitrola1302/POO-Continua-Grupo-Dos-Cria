0a5f609532355db5afac045818e1925338d66c3f
Mavebibo <112356542+Mavebibo@users.noreply.github.com>
2023-04-06 21:01:10 -0300

    Add files via upload
    Update dos pacotes

---
+++ AtividadeContinuada/src/br/gov/cesarschool/poo/fidelidade/cartao/dao/LancamentoExtratoDAO.java
@@ -0,0 +1,14 @@
+package br.gov.cesarschool.poo.fidelidade.cartao.dao;
+
+import br.gov.cesarschool.poo.fidelidade.cartao.entidade.LancamentoExtrato;
+
+public class LancamentoExtratoDAO {
+
+    public boolean incluir(LancamentoExtrato lancamento){
+        return false;
+    }
+
+    public boolean alterar(LancamentoExtrato lancamento){
+        return false;
+    }
+}
