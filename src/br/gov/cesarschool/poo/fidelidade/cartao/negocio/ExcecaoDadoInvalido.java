656fe9f6f7da860706088b4e7fa64f2ae57cc427
Vitrola1302 <vag@cesar.school>
2023-06-14 17:02:23 -0300

    criando exception dado invalido

---
+++ src/br/gov/cesarschool/poo/fidelidade/cartao/negocio/ExcecaoDadoInvalido.java
@@ -0,0 +1,7 @@
+package br.gov.cesarschool.poo.fidelidade.cartao.negocio;
+
+public class ExcecaoDadoInvalido extends Exception{
+	public ExcecaoDadoInvalido(String message) {
+		super(message);
+	}
+}
